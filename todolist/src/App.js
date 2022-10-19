6b313baf2df4d30a6d8ceae6bfe49173b116074b
SeoHee3478 <seohee3478@gmail.com>
2022-10-19 15:33:20 +0900

    feat: redux를 활용하여 insert와 remove, check가 가능한 TodoList 기능 구현

---
+++ todolist/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Todos from "./components/Todos";
+import TodosContainer from "./containers/TodosContainer";
+
+const App = () => {
+  return (
+    <div>
+      <TodosContainer />
+    </div>
+  );
+};
+
+export default App;
